70e62707946724adb94ae8aaf46cb47861423ca8
Harper <mharper@ciphertechsolutions.com>
2023-10-24 16:53:39 -0400

    Seperated out the GeoFetch read logic from the CSV read in prep for adding in the ability to read in multiple types of CSV files

--- Mongrel_CORE/Inputs/CsvRead.cs
+++ Mongrel_CORE/Inputs/CsvRead.cs
@@ -1,30 +1,11 @@
 ﻿using System.Globalization;
 using CsvHelper;
 using Yipper;
-using static Mongrel.Common.Utils;
+using static Mongrel.Inputs.ReportReaders.Csv.GeoFetch;
 
 namespace Mongrel.Inputs;
 
-public class GeoFetchHeaders
-{
-    public int Id { get; set; }
-    public string SOFEX { get; set; }
-    public string Exhibit { get; set; }
-    public string DeviceType { get; set; }
-    public string Filename { get; set; }
-    public string Hash { get; set; }
-    public string Path { get; set; }
-    public string Filetype { get; set; }
-    public string Timestamp { get; set; }
-    public string Longitude { get; set; }
-    public string Latitude { get; set; }
-    public string MGRS { get; set; }
-    public string Altitude { get; set; }
-    public string AltitudeMode { get; set; }
-    public string GeometryType { get; set; }
-    public string DeviceMake { get; set; }
-    public string DeviceModel { get; set; }
-}
+
 
 internal class CsvRead : Reader
 {
@@ -72,62 +53,7 @@ internal class CsvRead : Reader
         File.WriteAllLines(CsvPath, lines);
     }
 
-    private static IEnumerable<Locations> ReadGeoFetchCsv(IReader csv, string reportFileName)
-    {
-        IEnumerable<GeoFetchHeaders> records;
-        try
-        {
-            Logger.Instance.Info("Attempting to read file as GeoFetch CSV");
-            records = csv.GetRecords<GeoFetchHeaders>();
-        }
-        catch
-        {
-            Logger.Instance.Info("File not detected as a GeoFetch CSV file");
-            yield break;
-        }
-
-        var (sofex, exhibit, deviceType) = ParseReportFileName(reportFileName);
-
-        foreach (var record in records)
-        {
-            if (string.IsNullOrEmpty(record.Longitude) && string.IsNullOrEmpty(record.Latitude))
-            {
-                yield return new Locations();
-                continue;
-            }
-
-            var normLon = NormalizeCordStr(record.Longitude);
-            var normLat = NormalizeCordStr(record.Latitude);
-
-            yield return new Locations
-            {
-                Sofex = record.SOFEX ?? sofex,
-                DeviceType = record.DeviceType ?? deviceType,
-                FileName = record.Filename,
-                Hash = record.Hash,
-                Path = record.Path,
-                TimeStr = record.Timestamp,
-                Mgrs = record.MGRS ?? GetMgrs(normLat, normLon),
-                Altitude = record.Altitude,
-                AltitudeMode = record.AltitudeMode,
-                Load = "",
-                SheetName = record.Filetype,
-                ColumnName = $"Type-{record.GeometryType}",
-                ReportType = "GeoFetch",
-                Deleted = "",
-                Bssid = "",
-                Ssid = "",
-                Notes = "",
-                Exhibit = record.Exhibit ?? exhibit,
-                Origin = reportFileName,
-                OriginalLon = record.Longitude,
-                OriginalLat = record.Latitude,
-                Timestamp = null,
-                ConvertedLon = normLon,
-                ConvertedLat = normLat
-            };
-        }
-    }
+    
 
     public override void Dispose() { }
 }
---
+++ Mongrel_CORE/Inputs/ReportReaders/Csv/GeoFetch.cs
@@ -0,0 +1,94 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Mongrel.Common.Utils;
+using Yipper;
+
+namespace Mongrel.Inputs.ReportReaders.Csv
+{
+    public class GeoFetchHeaders
+    {
+        public int Id { get; set; }
+        public string SOFEX { get; set; }
+        public string Exhibit { get; set; }
+        public string DeviceType { get; set; }
+        public string Filename { get; set; }
+        public string Hash { get; set; }
+        public string Path { get; set; }
+        public string Filetype { get; set; }
+        public string Timestamp { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string MGRS { get; set; }
+        public string Altitude { get; set; }
+        public string AltitudeMode { get; set; }
+        public string GeometryType { get; set; }
+        public string DeviceMake { get; set; }
+        public string DeviceModel { get; set; }
+    }
+
+    public class GeoFetch
+    {
+        public static IEnumerable<Locations> ReadGeoFetchCsv(IReader csv, string reportFileName)
+        {
+            IEnumerable<GeoFetchHeaders> records;
+            try
+            {
+                Logger.Instance.Info("Attempting to read file as GeoFetch CSV");
+                records = csv.GetRecords<GeoFetchHeaders>();
+
+                var testCsvRecordHeaders = csv.GetRecords<CsvRecord>();
+            }
+            catch
+            {
+                Logger.Instance.Info("File not detected as a GeoFetch CSV file");
+                yield break;
+            }
+
+            var (sofex, exhibit, deviceType) = ParseReportFileName(reportFileName);
+
+            foreach (var record in records)
+            {
+                if (string.IsNullOrEmpty(record.Longitude) && string.IsNullOrEmpty(record.Latitude))
+                {
+                    yield return new Locations();
+                    continue;
+                }
+
+                var normLon = NormalizeCordStr(record.Longitude);
+                var normLat = NormalizeCordStr(record.Latitude);
+
+                yield return new Locations
+                {
+                    Sofex = record.SOFEX ?? sofex,
+                    DeviceType = record.DeviceType ?? deviceType,
+                    FileName = record.Filename,
+                    Hash = record.Hash,
+                    Path = record.Path,
+                    TimeStr = record.Timestamp,
+                    Mgrs = record.MGRS ?? GetMgrs(normLat, normLon),
+                    Altitude = record.Altitude,
+                    AltitudeMode = record.AltitudeMode,
+                    Load = "",
+                    SheetName = record.Filetype,
+                    ColumnName = $"Type-{record.GeometryType}",
+                    ReportType = "GeoFetch",
+                    Deleted = "",
+                    Bssid = "",
+                    Ssid = "",
+                    Notes = "",
+                    Exhibit = record.Exhibit ?? exhibit,
+                    Origin = reportFileName,
+                    OriginalLon = record.Longitude,
+                    OriginalLat = record.Latitude,
+                    Timestamp = null,
+                    ConvertedLon = normLon,
+                    ConvertedLat = normLat
+                };
+            }
+        }
+    }
+}
